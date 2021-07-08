ed5f0ed7e6d98c68b75035fd6ed490bfe1a5ac8c
Matan Omesi <matanfxp@hotmail.co.il>
2021-07-08 18:32:19 +0300

    remove dummy

--- src/app/core/models/weather.model.ts
+++ src/app/core/models/weather.model.ts
@@ -1,28 +1,6 @@
 import { ConditionsResult } from "./conditions.model";
 import { WeeklyForecast } from "./daily.forecast.model";
 
-export interface Country{
-  ID: string;
-  localizedName: string;
-};
-
-export interface AdministrativeArea{
-  ID: string;
-  localizedName: string;
-};
-
-export interface Weather2{
-  version: number;
-  key: string;
-  type: string;
-  rank: number;
-  localizedName: string;
-  country: Country;
-  administrativeArea: AdministrativeArea;
-}
-
-
-
 
 export interface Weather{
   ID: string;
